262432fd32bf43958050bd4ac44d39bfefc4e95b
noravazquez <vazquez.nora.g@gmail.com>
2023-11-26 19:43:44 -0600

    Error resuelto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav>Navbar</nav>
+      <Outlet />
+      
+    </>
+  )
+}
+
+export default App
