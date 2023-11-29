778637ca6c736bc0f98d50050ba8a7a7ab90c59d
Ubuntu <ubuntu@ip-172-31-38-179.ec2.internal>
2023-11-29 00:23:33 +0000

    intento

--- backend/src/models/usuarios.js
+++ backend/src/models/usuarios.js
@@ -1,7 +1,8 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 
 module.exports = function(sequelize) {
-  return sequelize.define('Usuario', {
+  class Usuario extends Model {}
+  Usuario.init({
     id_usuario: {
       autoIncrement: true,
       type: DataTypes.INTEGER,
@@ -22,6 +23,7 @@ module.exports = function(sequelize) {
     }
   }, {
     sequelize,
+    modelName: 'Usuario',  // Agregamos esta línea para especificar el nombre del modelo
     tableName: 'usuarios',
     schema: 'public',
     timestamps: false,
@@ -35,4 +37,7 @@ module.exports = function(sequelize) {
       },
     ]
   });
+
+  console.log('Métodos en Usuario:', Object.keys(Usuario.prototype));
+  return Usuario;
 };
