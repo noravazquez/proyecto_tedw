f808042cbf09ec0c34cff124b120a74b6f9558c3
klyct <takanashirikka.42@gmail.com>
2023-11-29 21:03:07 -0600

    orders chart

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize');
+const sequelize  = require('sequelize');
   const Cliente = require('../models/clientes');
   const Carrito = require('../models/carritos');
   const Producto = require('../models/productos');
