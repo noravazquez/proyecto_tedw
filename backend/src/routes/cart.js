f4053c02c8770e0aaf15042bb86137e2696db2da
klyct <takanashirikka.42@gmail.com>
2023-12-01 20:56:03 -0600

    eliminar del carrito

--- backend/src/routes/cart.js
+++ backend/src/routes/cart.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { agregarAlCarrito, obtenerCarrito, aplicarCuponDescuento } = require('../controllers/cartCotroller');
+const { agregarAlCarrito, obtenerCarrito, aplicarCuponDescuento, eliminarCantidadDelCarrito, eliminarDelCarrito } = require('../controllers/cartCotroller');
 
 router.post('/agregar-al-carrito/:idProducto', agregarAlCarrito);
 router.get('/carrito', obtenerCarrito);
 router.get('/cupon', aplicarCuponDescuento);
+router.post('/eliminar-del-carrito/:idProducto', eliminarDelCarrito);
+router.post('/elimina-de-carrito/:idProducto', eliminarCantidadDelCarrito);
 
 module.exports = router;
