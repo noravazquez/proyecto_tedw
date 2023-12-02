22850a403bbbf25c9dadb658f9e03d9b473a35c7
klyct <takanashirikka.42@gmail.com>
2023-12-02 17:34:42 -0600

    Reportes: Prodcuctos vendidos, clientes registrados

--- backend/src/routes/reports.js
+++ backend/src/routes/reports.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/authMiddleware');
-const { totalVentasAnual, estadisticasClientes, totalVentasMensual ,totalVentasSemanal} = require('../controllers/reportsController');
+const { totalVentasAnual, estadisticasClientes, totalVentasMensual, totalVentasSemanal, totalProductos, totalClientes} = require('../controllers/reportsController');
 
 //router.get('/total-ventas-anual', totalVentasAnual);
 //router.get('/estadisticas-clientes', estadisticasClientes);
 //router.get('/total-ventas-mensual', totalVentasMensual);
 //router.get('/total-ventas-semanal', totalVentasSemanal);
+//router.get('/total-productos-vendidos', totalProductos);
+//router.get('/total-clientes-registrados', totalClientes);totalClientes
 
 module.exports = router;
