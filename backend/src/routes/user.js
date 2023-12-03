716ac03cbcd273c7087f56d3fe5ca37983f1ac86
noravazquez <vazquez.nora.g@gmail.com>
2023-12-03 15:44:41 -0600

    user

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -4,11 +4,18 @@ const { getUserInfo, getUserOrders, generateOrderChart, updateClientInfo, update
 
 const router = express.Router();
 
-router.get('/profile', authorize(2), getUserInfo);
+/*router.get('/profile', authorize(2), getUserInfo);
 router.post('/datos', authorize(2),registerClient)
 router.get('/orders', authorize(2), getUserOrders);
 router.get('/order-chart', authorize(2), generateOrderChart);
 router.patch('/info-client', authorize(2), updateClientInfo);
-router.patch('/info-user', authorize(2), updateUserInfo); 
+router.patch('/info-user', authorize(2), updateUserInfo);*/ 
+
+router.get('/profile', getUserInfo);
+router.post('/datos',registerClient)
+router.get('/orders', getUserOrders);
+router.get('/order-chart', generateOrderChart);
+router.patch('/info-client', updateClientInfo);
+router.patch('/info-user', updateUserInfo); 
 
 module.exports = router;
