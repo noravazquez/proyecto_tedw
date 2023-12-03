521a8215b9a8f27189fd0215acced2fe2cf6e066
noravazquez <vazquez.nora.g@gmail.com>
2023-12-03 15:25:36 -0600

    cards totales

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -5,8 +5,8 @@ const { getAllCategorias, getCategoriaById, createCategoria, deleteCategoria, up
 const router = express.Router();
 
 //Rutas Para cruds categoria
-router.get('/categorias', authorize(1), getAllCategorias);
-router.get('/categoria/:id', authorize(1), getCategoriaById);
+router.get('/categorias', getAllCategorias);
+router.get('/categoria/:id', getCategoriaById);
 router.post('/categoria', authorize(1), createCategoria);
 router.put('/categoria/:id', authorize(1), updateCategoria);
 router.delete('/categoria/:id', authorize(1), deleteCategoria);
