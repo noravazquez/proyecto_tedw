59c89a537294b7c8b1c6ea88223b81c32c3f1a3d
noravazquez <vazquez.nora.g@gmail.com>
2023-12-03 20:25:25 -0600

    producto admin

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,10 +2,10 @@ const { Sequelize } = require('sequelize');
 //const SequelizeAuto = require('sequelize-auto');
 
 const sequelize = new Sequelize({
-  database: 'innovatech',
-  username: 'admin_innovatech',
-  password: '1234',
-  host: 'localhost',
+  database: 'BDTEDW',
+  username: 'postgres',
+  password: 'PfGHuuGJZw0A1CSt7tHt',
+  host: 'db1.cfyu3whtuwpf.us-east-1.rds.amazonaws.com',
   port: '5432',
   dialect: 'postgres',
   define: {
